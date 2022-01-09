e8fd77879b57dbffac8a5a50cf3a711948c86052
leticianardi <leticia.tnardi@gmail.com>
2022-01-09 00:51:35 -0800

    readmefile

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
-# Note Taker Starter Code
+# Note Taker
 
-## Packages
-Install the following packages:
+This applications allows the user to write, save and delete simple notes.
+
+This app has been deployed on [Heroku](https://evening-coast-28377.herokuapp.com/).
+
+## Built With
+
+- Express.js
+- UUID
+- Heroku
+- HTML
+- Advanced CSS styles / Bootstrap
+- JavaScript
+- NPM modules - Express, fs, path
+
+## Installation
+
+To function properly, install the following packages:
+
 ```
 $ npm init
-$ npm install express --save-dev
+$ npm install express --save-dev uuid
 ```
+
+## Snaphsot
+
+<img src="public/assets/images/Screenshot 1.png" />
+<img src="public/assets/images/Screenshot 2.png" />
+
+## Questions
+
+More Questions? Contact me via:  
+GitHub: [@leticianardi](https://github.com/leticianardi)
--- server.js
+++ server.js
@@ -15,27 +15,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 //APIs ROUTES
-
-// get route
+// get route > find notes
 app.get('/api/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/db/db.json'));
 });
 
-// post route
+// post route > write new note
 app.post('/api/notes', (req, res) => {
-  const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+  const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
   const newNotes = req.body;
+
   newNotes.id = uuid.v4();
   notes.push(newNotes);
   fs.writeFileSync('./db/db.json', JSON.stringify(notes));
   res.json(notes);
 });
 
-// delete routes
+// delete route > delete note
 app.delete('/api/notes/:id', (req, res) => {
-  const notes = JSON.parse(fs.readFileSync('./db/db.json'));
-
+  const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
   const deleteNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+
   fs.writeFileSync('./db/db.json', JSON.stringify(deleteNote));
   res.json(deleteNote);
 });
